0fb54af94f86130d03ecaff32db8121b8432fa31
Tomos Slater <Tomos.Slater@hotmail.co.uk>
2018-12-05 22:50:45 +0000

    Added newer welcome.c

--- welcome.c
+++ welcome.c
@@ -3,7 +3,7 @@ welcome()
 	//printing welcome message
 	printf("--------------------------------");
 	printf("\n|                              |");
-	printf("\n|  Welcome to the Meme Search  |");
+	printf("\n|  Welcome to the Wordsearch!  |");
 	printf("\n|                              |");
 	printf("\n--------------------------------\n");
 }
